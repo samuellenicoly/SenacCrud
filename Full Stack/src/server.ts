c25a9dccc50d18d71eeb7c5ecb3b27c5dd4d2711
samuelle <samuellenicolyfagundes@gmail.co>
2025-10-28 17:27:47 -0300

    Versionamento

---
+++ Full Stack/src/server.ts
@@ -0,0 +1,8 @@
+import express , {Request , Response , NextFunction} from 'express'
+import 'express-assync-errors'
+import cors from 'cors'
+
+const app = express()
+app.use(express.json())
+app.use(cors())
+
