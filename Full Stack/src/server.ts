1d3897043d26eccdf46a4df55addb8eb700d56b2
Samuelle Nicoly <samuellenicolyfagundes@gmail.com>
2025-10-29 15:37:23 -0300

    29-10-2025

---
+++ Full Stack/src/server.ts
@@ -0,0 +1,20 @@
+import express, { Request, Response, NextFunction } from 'express'
+import 'express-async-errors'
+import cors from 'cors'
+
+const app = express()
+app.use(express.json())
+app.use(cors())
+
+app.use((err: Error, req: Request, res: Response, next: NextFunction) => {
+    if(err instanceof Error){
+        return res.status(400).json({
+            error: err.message
+        })
+    }
+    return res.status(500).json({
+        status: 'Erro',
+        message: 'Erro Interno do Servidor'
+    })
+})
+app.listen(3333, () => console.log('Servidor On-Line na porta 3333'))
